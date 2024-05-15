3d053cdc309f3a2f4c25d0249d6736a358283a2f
AINER <den.djomin@mail.ru>
2024-05-15 19:57:18 +0300

    Step 2: Add arguments with commander.js
    Added argument description with commander.js help in gendiff.js file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,8 @@ program
   .name('gendiff')
   .version('0.0.1')
   .description('CLI to some JavaScript string utilities')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format', 'output format')
 
 program.parse();
